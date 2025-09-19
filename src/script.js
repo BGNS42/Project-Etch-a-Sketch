1f93726b2df6f5f54453e698f51c4a2be64b08a9
BGNS42 <196232533+BGNS42@users.noreply.github.com>
2025-09-19 20:54:36 -0300

    maximum grid range and new squareSize logic

--- src/script.js
+++ src/script.js
@@ -16,32 +16,37 @@ grid.appendChild(board);
 
 
 // square creation
-function squareCreation(grid) {
-    const size = grid * grid;
-    const pixels = 500 / grid;
+function squareCreation(gridCount) {
+    const size = gridCount * gridCount;
+    const boardSize = 798;
+    const squareSize = (boardSize / gridCount);
     for (let i = 0; i < size; i++) {
         const square = document.createElement("div");
         square.classList.add("square");
-        square.style.width = `${pixels}px`;
-        square.style.height = `${pixels}px`;
-        board.appendChild(square);
+        square.style.width = `${squareSize}px`;
+        square.style.height = `${squareSize}px`;
+        //square.style.fontSize = "8px"
+        //square.textContent = `${i + 1}`;
+
+        // paint the mouse trail
         square.addEventListener("mouseout", () => {
             square.style.backgroundColor = "lightblue";
         });
+        board.appendChild(square);
     }
-    //console.log(`Square Size: ${pixels} px`);
+    //console.log(`Square Size: ${squareSize} px`);
 }
 
 squareCreation(16);
 
 const userInput = () => {
-    return parseInt(prompt("Select grid size (Maximum 50)"));   
+    return parseInt(prompt("Select grid size (Maximum 100)"));   
 }
 
 function newGrid() {
     const size = userInput();
-    if(size && size > 0 && size <= 50) {
-        //limpa o board e carrega novos quadrados
+    if(size && size > 0 && size <= 100) {
+        //clean the previous board and create a new one
         board.innerHTML = "";
         squareCreation(size);
     } else {
